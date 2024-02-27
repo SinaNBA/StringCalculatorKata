7ab5021b0d95ddd1bee34640f8b277cc35b70ff4
Sina Nejadbakhtiari <sina.nbakhtiari@gmail.com>
2024-02-27 16:04:48 +0100

    Step 3

--- StringCalculatorKata/Program.cs
+++ StringCalculatorKata/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"The sum is(with ','): {Add("1,\n2,3")}");
-            Console.WriteLine($"The sum is(with ';'): {Add2("//;\n1;2")}");
+            Console.WriteLine($"The sum is(with ',') for step1: {Add("1,2,3")}");
+            Console.WriteLine($"The sum is(with ',')for step3: {Add("1,\n2,3")}");
         }
         private static int Add(string numbersString)
         {
@@ -15,29 +15,7 @@
             numbersString = numbersString.Replace("\n", ",");
             string[] numbers = numbersString.Split(delimiterChars, System.StringSplitOptions.RemoveEmptyEntries);            
 
-            //foreach (string number in numbers)
-            //{
-            //    if(!string.IsNullOrEmpty(number))sum += Convert.ToInt32(number);
-            //}
-
             return numbers.Sum(int.Parse);
-        }
-        static int Add2(string numbers)
-        {
-            if (string.IsNullOrEmpty(numbers))
-                return 0;
-
-            string delimiter = ",";
-            if (numbers.StartsWith("//"))
-            {
-                var delimiterLine = numbers.Substring(2, numbers.IndexOf('\n') - 2);
-                delimiter = delimiterLine;
-                numbers = numbers.Substring(numbers.IndexOf('\n') + 1);
-            }
-
-            numbers = numbers.Replace("\n", delimiter);
-            var nums = numbers.Split(delimiter.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-            return nums.Sum(int.Parse);
         }
 
     }
