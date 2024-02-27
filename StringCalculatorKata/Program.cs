3620850175031c27c1d521e3d322f061d71b43b8
Sina Nejadbakhtiari <sina.nbakhtiari@gmail.com>
2024-02-27 17:52:15 +0100

    Step 5

--- StringCalculatorKata/Program.cs
+++ StringCalculatorKata/Program.cs
@@ -1,12 +1,15 @@
-﻿namespace StringCalculatorKata
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace StringCalculatorKata
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine($"The sum is(with ',') for step1: {Add("1,2,3")}");
-            //Console.WriteLine($"The sum is(with ',')for step3: {Add("1,\n2,3")}");
+            Console.WriteLine($"The sum is(with ',') for step1: {Add("1,2,3")}");
+            Console.WriteLine($"The sum is(with ',')for step3: {Add("1,\n2,3")}");
             Console.WriteLine($"The sum is(with ',')for step4: {Add("//;\n1;2")}");
+            Console.WriteLine($"The sum is(with ',')for step4: {Add("//;\n1;2;-1;-2;-3")}");
         }
         private static int Add(string numbersString)
         {
@@ -17,6 +20,18 @@
 
             numbersString = numbersString.Replace("\n", ",");
             string[] numbers = numbersString.Split(delimiterChars, System.StringSplitOptions.RemoveEmptyEntries);
+
+            //to check if there are any negative numbers
+            int negativeCount = 0;
+            foreach (string number in numbers)
+            {
+                if (int.Parse(number) < 0) negativeCount++;
+            }
+            if (negativeCount > 0)
+            {
+                var negatives = numbers.Where(num => int.Parse(num) < 0).ToList();
+                throw new Exception($"negatives not allowed, the negative numbers is: {string.Join(',',negatives)}");
+            }
 
             return numbers.Sum(int.Parse);
         }
