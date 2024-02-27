aeda47e9ea9ab25f430a7185c6577abde353b09c
Sina Nejadbakhtiari <sina.nbakhtiari@gmail.com>
2024-02-27 17:23:51 +0100

    Step 4

--- StringCalculatorKata/Program.cs
+++ StringCalculatorKata/Program.cs
@@ -4,16 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"The sum is(with ',') for step1: {Add("1,2,3")}");
-            Console.WriteLine($"The sum is(with ',')for step3: {Add("1,\n2,3")}");
+            //Console.WriteLine($"The sum is(with ',') for step1: {Add("1,2,3")}");
+            //Console.WriteLine($"The sum is(with ',')for step3: {Add("1,\n2,3")}");
+            Console.WriteLine($"The sum is(with ',')for step4: {Add("//;\n1;2")}");
         }
         private static int Add(string numbersString)
         {
             if (string.IsNullOrEmpty(numbersString)) return 0;
-            char[] delimiterChars = { ',', ';'};
+            char[] delimiterChars = { ',', ';' };
+
+            if (numbersString.StartsWith("//")) numbersString = numbersString.Substring(3);
 
             numbersString = numbersString.Replace("\n", ",");
-            string[] numbers = numbersString.Split(delimiterChars, System.StringSplitOptions.RemoveEmptyEntries);            
+            string[] numbers = numbersString.Split(delimiterChars, System.StringSplitOptions.RemoveEmptyEntries);
 
             return numbers.Sum(int.Parse);
         }
